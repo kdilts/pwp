e4bb5395dd5b89335178dd99647678e5be0f97a0
Kevin Dilts <kdilts13@gmail.com>
2016-10-12 08:40:43 -0600

    created basic layout with headers and paragraphs for purpose audience and goal. currently with filler text

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,24 @@
 		<title>kdilts PWP</title>
 	</head>
 	<body>
-
+		<header>
+			<h1>kdilts PWP</h1>
+		</header>
+		<main>
+			<section>
+				<h2>Purpose</h2>
+				<p>asd;flkjasdf;lkajsfd;laskdjfa;sldkfjas;ldfkjasd;lfkj</p>
+			</section>
+			<hr/>
+			<section>
+				<h3>Audience</h3>
+				<p>asd;flkjasdf;lkajsfd;laskdjfa;sldkfjas;ldfkjasd;lfkj</p>
+			</section>
+			<hr/>
+			<section>
+				<h4>Goal</h4>
+				<p>asd;flkjasdf;lkajsfd;laskdjfa;sldkfjas;ldfkjasd;lfkj</p>
+			</section>
+		</main>
 	</body>
 </html>
