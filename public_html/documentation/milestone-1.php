ded0ea1e8ce9c5d797476e2d5e607bc8f62293ef
Kevin Dilts <kdilts13@gmail.com>
2016-10-12 08:34:35 -0600

    changed indents from spaces to tabs

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8"/>
-    <title>kdilts PWP</title>
-  </head>
-  <body></body>
+	<head>
+		<meta charset="utf-8"/>
+		<title>kdilts PWP</title>
+	</head>
+	<body>
+
+	</body>
 </html>
