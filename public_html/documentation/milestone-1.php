2f29aacf90ae2ce3ba96450c22b8c8bffd24fde4
Kevin Dilts <kdilts13@gmail.com>
2016-10-15 18:27:08 -0600

    started filling out each category. changed color scheme

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,32 +7,40 @@
 	</head>
 	<body>
 		<header>
-			<h1>kdilts PWP</h1>
+			<h1 id="titleheader">kdilts PWP</h1>
 		</header>
 		<main>
 			<section>
 				<h2 class="subheading">Purpose</h2>
-				<p>asd;flkjasdf;lkajsfd;laskdjfa;sldkfjas;ldfkjasd;lfkj</p>
+				<p>The purpose of this website is to serve as a portfolio and showcase some of my prior work.</p>
 			</section>
-			<hr/>
+
 			<section>
 				<h3 class="subheading">Audience</h3>
-				<p>asd;flkjasdf;lkajsfd;laskdjfa;sldkfjas;ldfkjasd;lfkj</p>
+				<p>My intended audience is primarily employers and / or prospective clients. Other audience members could be peers, or family and friends that I would like to show my work to.</p>
 			</section>
-			<hr/>
+
 			<section>
 				<h4 class="subheading">Goal</h4>
-				<p>asd;flkjasdf;lkajsfd;laskdjfa;sldkfjas;ldfkjasd;lfkj</p>
+				<p>My goal is to create a nice looking gallery that will leave people with a good impression of my web design skills.</p>
 			</section>
-			<hr/>
+
 			<section>
 				<h5 class="subheading">Persona</h5>
-				<p>asd;flkjasdf;lkajsfd;laskdjfa;sldkfjas;ldfkjasd;lfkj</p>
+				<ul>
+					<li>Name:</li>
+					<li>Age:</li>
+					<li>Profession:</li>
+					<li>Technology:</li>
+					<li>Attitudes and behaviors:</li>
+					<li>Frustrations and needs:</li>
+					<li>Goals:</li>
+				</ul>
 			</section>
-			<hr/>
+
 			<section>
 				<h6 class="subheading">Use Case</h6>
-				<p>asd;flkjasdf;lkajsfd;laskdjfa;sldkfjas;ldfkjasd;lfkj</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac urna in leo malesuada luctus. Integer malesuada lacus ligula. Sed laoreet blandit risus in blandit. Donec eleifend ultrices elit, commodo facilisis nunc dictum ut. Etiam pulvinar scelerisque diam, quis finibus nulla pretium quis. Nullam gravida condimentum porta. Vivamus ultricies, velit quis tristique tempor, dui lacus commodo sem, at dapibus purus nulla in elit. In hac habitasse platea dictumst. Maecenas tincidunt suscipit tortor, vitae vulputate est facilisis facilisis. Praesent laoreet sem ut diam viverra, nec aliquet odio aliquam. Vivamus id felis sit amet sem sodales elementum. Ut imperdiet diam eget sollicitudin vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			</section>
 		</main>
 	</body>
