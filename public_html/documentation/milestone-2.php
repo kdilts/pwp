2424f42433cdce67bd9a5ad5f2021761cf846150
Kevin Dilts <kdilts13@gmail.com>
2016-10-20 10:47:23 -0600

    creating layout for content to go into

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -13,11 +13,35 @@
 			<section>
 				<h2 class="subheading">Content Strategy</h2>
 				<p>
-					testing
+					I plan on creating a gallery of my old projects to show potential employers. This site will be multi-page, with one page devoted to each piece of my prior work. I would also like to have an about me section on the home page that features my contact info, alink to my resume, and my professional photo, as well animated pull down nav bar. It would be nice to have a couple of pages that will show screenshots of each project in a particular category, for example, an astronomy page with a picture, link, and small description of each of my astronomy projects.
 				</p>
 			</section>
 			<section>
 				<h3 class="subheading">Wire Frames</h3>
+				<h4>Home Page</h4>
+				<div class="flexContainer">
+					<div>
+						<h5>Desktop</h5>
+						<img src="#" style="border:2px solid black; width:300px; height:300px;"/>
+					</div>
+					<div>
+						<h5>Mobile</h5>
+						<img src="#" style="border:2px solid black; width:300px; height:300px;"/>
+					</div>
+				</div>
+				<h4>List Page</h4>
+				<div class="flexContainer">
+					<div>
+						<h5>Desktop</h5>
+						<img src="#" style="border:2px solid black; width:300px; height:300px;"/>
+					</div>
+					<div>
+						<h5>Mobile</h5>
+						<img src="#" style="border:2px solid black; width:300px; height:300px;"/>
+					</div>
+				</div>
+				<h4>Project Page</h4>
+				<p>No wire frame for this one. I will be importing projects that I have already finished. Because these projects were designed to be run on UNM's desktop computers, many of them will not be mobile friendly.</p>
 			</section>
 		</main>
 	</body>
