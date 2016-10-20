137d77892759d17ed8363b795af8a5eef09ef801
Kevin Dilts <kdilts13@gmail.com>
2016-10-20 09:19:16 -0600

    created mostly blank page for mile stone 2 alpha content

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>kdilts PWP</title>
+		<link href="../css/stylesheet.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body>
+		<header>
+			<h1 id="titleheader">kdilts PWP</h1>
+		</header>
+		<main>
+
+		</main>
+	</body>
+</html>
