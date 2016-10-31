efb19ecb52ce91555b6495836c83e67f7b56e84b
Kevin Dilts <kdilts13@gmail.com>
2016-10-31 09:08:16 -0600

    removed one more hotlinked image that i missed

--- public_html/documentation/cats.php
+++ public_html/documentation/cats.php
@@ -2,9 +2,6 @@
 	<h3 class="subheading">Flickity test</h3>
 	<div class="carousel" data-flickity='{ "wrapAround": true }'>
 		<div class="carousel-cell">
-			<img src="http://r.ddmcdn.com/s_f/o_1/cx_0/cy_0/cw_300/ch_300/w_300/APL/uploads/2014/10/kitten-cuteness300.jpg"/>
-		</div>
-		<div class="carousel-cell">
 			<img src="../documentation/images/cat1.jpg"/>
 		</div>
 		<div class="carousel-cell">
