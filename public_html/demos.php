92480974c2a7ed645fad75e7b21e7cfa55ba59cc
Kevin Dilts <kdilts13@gmail.com>
2016-11-08 15:33:32 -0700

    added images and titles to cells in demos.php using place holder descriptions

--- public_html/demos.php
+++ public_html/demos.php
@@ -23,42 +23,42 @@
 						<div class="carousel" id="flick" data-flickity='{ "wrapAround": true }'>
 
 							<div class="carousel-cell cell-text">
-								<img src="#" class="img-rounded"/>
+								<img src="images/conway.jpg" class="img-rounded"/>
 								<div class="cell-description">
-									<h1></h1>
-									<p></p>
+									<h1>Conway's Game of Life</h1>
+									<p>conway</p>
 								</div>
 							</div>
 
 							<div class="carousel-cell cell-text">
-								<img src="#" class="img-rounded"/>
+								<img src="images/tetris.jpg" class="img-rounded"/>
 								<div class="cell-description">
-									<h1></h1>
-									<p></p>
+									<h1>Tetris</h1>
+									<p>tetris</p>
 								</div>
 							</div>
 
 							<div class="carousel-cell cell-text">
-								<img src="#" class="img-rounded"/>
+								<img src="images/snake.jpg" class="img-rounded"/>
 								<div class="cell-description">
-									<h1></h1>
-									<p></p>
+									<h1>Snake</h1>
+									<p>snake</p>
 								</div>
 							</div>
 
 							<div class="carousel-cell cell-text">
-								<img src="#" class="img-rounded"/>
+								<img src="images/convex.jpg" class="img-rounded"/>
 								<div class="cell-description">
-									<h1></h1>
-									<p></p>
+									<h1>Convex Hull Problem</h1>
+									<p>convex</p>
 								</div>
 							</div>
 
 							<div class="carousel-cell cell-text">
-								<img src="#" class="img-rounded"/>
+								<img src="images/wsearch.jpg" class="img-rounded"/>
 								<div class="cell-description">
-									<h1></h1>
-									<p></p>
+									<h1>Word Search Generator</h1>
+									<p>word search</p>
 								</div>
 							</div>
 
