5cc092b408bd6442f0a2a0557ffbff0b3fbad4dd
Kevin Dilts <kdilts13@gmail.com>
2016-11-08 15:21:46 -0700

    added blank java page

---
+++ java.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php
+		require_once("lib/head_utils.php");
+		?>
+		<link rel="stylesheet" href="css/stylesheet.css" type="text/css"/>
+		<script src="javascript/cellDescriptionVisiblity.js" rel="script" type="text/javascript"></script>
+		<title>Java Downloads Gallery</title>
+	</head>
+	<body>
+
+		<header>
+			<?php
+			require_once("lib/navbar.php");
+			?>
+		</header>
+
+		<main>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="carousel" id="flick" data-flickity='{ "wrapAround": true }'>
+
+							<div class="carousel-cell cell-text">
+								<img src="#" class="img-rounded"/>
+								<div class="cell-description">
+									<h1></h1>
+									<p></p>
+								</div>
+							</div>
+
+							<div class="carousel-cell cell-text">
+								<img src="#" class="img-rounded"/>
+								<div class="cell-description">
+									<h1></h1>
+									<p></p>
+								</div>
+							</div>
+
+							<div class="carousel-cell cell-text">
+								<img src="#" class="img-rounded"/>
+								<div class="cell-description">
+									<h1></h1>
+									<p></p>
+								</div>
+							</div>
+
+							<div class="carousel-cell cell-text">
+								<img src="#" class="img-rounded"/>
+								<div class="cell-description">
+									<h1></h1>
+									<p></p>
+								</div>
+							</div>
+
+							<div class="carousel-cell cell-text">
+								<img src="#" class="img-rounded"/>
+								<div class="cell-description">
+									<h1></h1>
+									<p></p>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+
+	</body>
+</html>
