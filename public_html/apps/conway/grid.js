0095c2c5d5cf876a76b265fdefe912c25a5379b7
Kevin Dilts <kdilts13@gmail.com>
2016-11-08 19:47:37 -0700

    added files for conways game of life

---
+++ public_html/apps/conway/grid.js
@@ -0,0 +1,142 @@
+grid = function(x,y){
+	this.sx = x;
+	this.sy = y;
+
+	this.tileWidth;
+	this.tileHeight;
+
+	this.content = [];
+
+	this.init = function(){
+		for(var i = 0; i < this.sx; i++){
+			this.content[i] = [];
+			for(var j = 0; j < this.sy; j++){
+				this.content[i][j] = 0;
+			}
+		}
+	}
+
+	this.randomize = function(){
+		for(var i = 0; i < this.sx; i++){
+			for(var j = 0; j < this.sy; j++){
+				if(Math.random() > .5){
+					this.content[i][j] = 0;
+				}else{
+					this.content[i][j] = 1;
+				}
+			}
+		}
+	}
+
+	this.disp = function(){
+		var str;
+		for(var i = 0; i < this.sx; i++){
+			str = '';
+			for(var j = 0; j < this.sy; j++){
+				str += this.content[i][j];
+				str += ' ';
+			}
+			console.log(str);
+			console.log('');
+		}
+	}
+
+	this.get = function(x,y){ return this.content[x][y]; }
+
+	this.getAdj = function(x,y,wrap){
+		var total = 0;
+
+		// calculate neighbor coords
+		var up = y-1;
+		var dn = y+1;
+		var lf = x-1;
+		var rt = x+1;
+
+		if(wrap){
+			// perform wrap if needed
+			if(up < 0){ up = this.sy-1; }
+			if(dn > this.sy-1){ dn = 0; }
+			if(lf < 0){ lf = this.sx-1; }
+			if(rt > this.sx-1){ rt = 0; }
+
+			// count neighbors
+			if(this.get(x,up)==1){ total++; }
+			if(this.get(x,dn)==1){ total++; }
+			if(this.get(lf,y)==1){ total++; }
+			if(this.get(rt,y)==1){ total++; }
+
+			if(this.get(lf,up)==1){ total++; }
+			if(this.get(lf,dn)==1){ total++; }
+			if(this.get(rt,up)==1){ total++; }
+			if(this.get(rt,dn)==1){ total++; }
+		}else{
+			// count neighbors
+			if(up !== -1 && this.get(x,up)==1){ total++; }
+			if(dn < this.sy && this.get(x,dn)==1){ total++; }
+			if(lf !== -1 && this.get(lf,y)==1){ total++; }
+			if(rt < this.sx && this.get(rt,y)==1){ total++; }
+
+			if(up !== -1 && lf !== -1 && this.get(lf,up)==1){ total++; }
+			if(dn < this.sy && lf !== -1 && this.get(lf,dn)==1){ total++; }
+			if(up !== -1 && rt < this.sx && this.get(rt,up)==1){ total++; }
+			if(dn < this.sy && rt < this.sx && this.get(rt,dn)==1){ total++; }			
+		}
+
+		return total;
+	}
+
+	this.set = function(x,y,s){ this.content[x][y] = s; }
+
+	this.toggle = function(x,y){
+		if(this.content[x][y] == 0){
+			this.content[x][y] = 1;
+		}else{
+			this.content[x][y] = 0;
+		}
+	}
+
+	this.draw = function(gfx,width,height){
+		// clear
+		gfx.fillStyle = 'rgb(100,100,100)';
+		gfx.fillRect(0,0,width,height);
+
+		// calculate size
+		this.tileWidth = width/this.sx;
+		this.tileHeight = height/this.sy;
+
+		// tiles
+		gfx.fillStyle = gfx.strokeStyle = 'green';
+		for(var i in this.content){
+			for(var j in this.content[i]){
+				if(this.content[i][j] == 1){
+					gfx.fillRect(
+						i*this.tileWidth,j*this.tileHeight,
+						this.tileWidth,this.tileHeight
+					);
+				}
+			}
+		}
+
+		// grid lines
+		gfx.fillStyle = gfx.strokeStyle = 'black';
+		gfx.lineWidth = 1;
+
+		// vertical
+		for(var i = 1; i < this.sx; i++){
+			gfx.beginPath();
+			gfx.moveTo(i*this.tileWidth,0);
+			gfx.lineTo(i*this.tileWidth,height);
+			gfx.stroke();
+		}
+
+		// horizontal
+		for(var i = 1; i < this.sy; i++){
+			gfx.beginPath();
+			gfx.moveTo(0,i*this.tileHeight);
+			gfx.lineTo(width,i*this.tileHeight);
+			gfx.stroke();
+		}
+
+	}
+
+}
---
+++ public_html/apps/conway/life.js
@@ -0,0 +1,118 @@
+var canvas;
+var gfx;
+var gameGrid;
+
+var cwidth;
+var cheight;
+
+var playing = false;
+var speed; var speedCount = 0;
+var wrap = true;
+
+var playButton;
+var clearButton;
+var randomizeButton;
+var wrapButton;
+var speedSlider;
+
+window.onload = function(){
+	canvas = document.getElementById('mycanvas');
+	canvas.width = 600;
+	canvas.height = 600;
+	cwidth = canvas.width;
+	cheight = canvas.height;
+	gfx = canvas.getContext('2d');
+
+	gameGrid = new grid(30,30);
+	gameGrid.init();
+	gameGrid.randomize();
+
+	playButton = document.getElementById('play');
+	clearButton = document.getElementById('clear');
+	randomizeButton = document.getElementById('randomize');
+	wrapButton = document.getElementById('wrap');
+
+	speedSlider = document.getElementById('speed');
+	speed = speedSlider.value;
+
+	canvas.onclick = function(e){
+		var clickX = e.offsetX;
+		var clickY = e.offsetY;
+
+		var tileX = Math.floor(clickX / gameGrid.tileWidth);
+		var tileY = Math.floor(clickY / gameGrid.tileHeight);
+
+		gameGrid.toggle(tileX,tileY);
+	}
+
+	playButton.onclick = function(){
+		if(playing){
+			playing = false;
+			playButton.value = "Play";
+		}else{
+			playing = true;
+			playButton.value = "Pause";
+		}
+	}
+
+	clearButton.onclick = function(){ gameGrid.init(); }
+
+	randomizeButton.onclick = function(){ gameGrid.randomize(); }	
+
+	wrapButton.onclick = function(){
+		if(wrap){
+			wrap = false;
+			wrapButton.value = "Enable Wrap";
+		}else{
+			wrap = true;
+			wrapButton.value = "Disable Wrap";
+		}
+	}
+
+	speedSlider.onchange = function(){
+		speed = speedSlider.value;
+		speedCount = 0;
+	}
+
+	setInterval(render, 1000/60);
+}
+
+
+
+window.onresize = function(){
+	cwidth = canvas.width;
+	cheight = canvas.height;
+}
+
+render = function(){
+	gameGrid.draw(gfx,cwidth,cheight);
+	if(playing){
+		if(speedCount == speed*10){ update(); speedCount = 0; }
+		speedCount++;
+	}
+}
+
+update = function(){
+	// <2 neighbors - die
+	// 2 or 3 neighbors - live
+	// >3 neighbors - die
+	// exactly 3 neighbors - come alive
+	var killList = [];
+	var addList = [];
+
+	for(var i = 0; i < gameGrid.sx; i++){
+		for(var j = 0; j < gameGrid.sy; j++){
+			var count = gameGrid.getAdj(i,j,wrap);
+			if(count < 2 || count > 3){ killList.push([i,j]); }
+			if(count == 3){ addList.push([i,j]); }
+		}
+	}
+
+	for(var i in killList){
+		gameGrid.set(killList[i][0],killList[i][1],0);
+	}
+
+	for(var i in addList){
+		gameGrid.set(addList[i][0],addList[i][1],1);
+	}
+}
