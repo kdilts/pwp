8a5caad9358c9a8769e0f0bd24a55f86a420bb29
Kevin Dilts <kdilts13@gmail.com>
2016-11-14 14:31:39 -0700

    created links for astronomy apps from gallery page

--- public_html/astronomy.php
+++ public_html/astronomy.php
@@ -29,7 +29,9 @@
 						<div class="carousel" id="flick" data-flickity='{ "wrapAround": true }'>
 
 							<div class="carousel-cell cell-text">
-								<img src="images/milkyway.jpg" class="img-rounded"/>
+								<a href="apps/milkyway/milkyway.php">
+									<img src="images/milkyway.jpg" class="img-rounded"/>
+								</a>
 								<div class="cell-description">
 									<h1>Milky Way Model</h1>
 									<p>This is a model of the Milky Way Galaxy. The user can can rotate the model by left clicking and dragging. The model can be zoomed with the scroll wheel, and panned by right clicking and dragging. There are buttons for each of these features on the side menu as well. The checkboxes on the side menu hide and show different groupings of stars. Each grouping is represented by a unique color in the model.</p>
@@ -37,7 +39,9 @@
 							</div>
 
 							<div class="carousel-cell cell-text">
-								<img src="images/solarsystem.jpg" class="img-rounded"/>
+								<a href="apps/solarsystem/solarsystem.php">
+									<img src="images/solarsystem.jpg" class="img-rounded"/>
+								</a>
 								<div class="cell-description">
 									<h1>Solar System Model</h1>
 									<p>This is a model of the Solar System. The user is able to rotate and zoom, as well as center the view on each of the different objects in the model. The different check boxes in the menu enable the user to toggle the name tags and orbit rings for each object.</p>
@@ -45,7 +49,9 @@
 							</div>
 
 							<div class="carousel-cell cell-text">
-								<img src="images/retrograde.jpg" class="img-rounded"/>
+								<a href="apps/retrograde/retrograde.php">
+									<img src="images/retrograde.jpg" class="img-rounded"/>
+								</a>
 								<div class="cell-description">
 									<h1>Retrograde Motion</h1>
 									<p>This program shows retrograde motion from two perspectives to help the user understand what is happening. The outermost ring with the stars is the sky view from whichever planet the user is looking from. It shows the positions of the sun and the planet the user is looking to, as seen from the surface of the first planet.
@@ -54,7 +60,9 @@
 							</div>
 
 							<div class="carousel-cell cell-text">
-								<img src="images/spectroscopic.jpg" class="img-rounded"/>
+								<a href="apps/spectroscopic/spectroscopic.php">
+									<img src="images/spectroscopic.jpg" class="img-rounded"/>
+								</a>
 								<div class="cell-description">
 									<h1>Spectroscopic Binaries</h1>
 									<p>This program demonstrates the behavior of binary stars, and the light they emit. The fiducial view and Earth view show the same pair of binary stars from different angles. Fiducial view can be thought of as looking straight down at the plane of the stars' orbit. Earth view is usually tilted at an angle to this plane. The graph shows the radial velocities of each star in the pair. These correspond the motion on the color bar below the graph. The color bar shows how the doppler effect changes the wavelength of light given off by each star as it moves towards or away from the observer on earth.</p>
@@ -81,7 +89,9 @@
 							</div>
 
 							<div class="carousel-cell cell-text">
-								<img src="images/eclipsing.jpg" class="img-rounded"/>
+								<a href="apps/eclipsing/eclipsing.php">
+									<img src="images/eclipsing.jpg" class="img-rounded"/>
+								</a>
 								<div class="cell-description">
 									<h1>Eclipsing Binaries</h1>
 									<p>This program demonstrates how the intensity of light given of by binary stars changes as they orbit each other. The user can change the angle of inclination, the distance between the stars, and how large the stars are. These factors will change how the stars eclipse each other, which will be reflected in the intensity graph.</p>
