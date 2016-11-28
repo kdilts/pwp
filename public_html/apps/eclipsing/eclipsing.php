81e2c30c382085c16dca36f6ad6b80b4d1966b35
Kevin Dilts <kdilts13@gmail.com>
2016-11-27 17:39:47 -0700

    changed eclipsing to use iframe

--- public_html/apps/eclipsing/eclipsing.php
+++ public_html/apps/eclipsing/eclipsing.php
@@ -11,47 +11,12 @@
 		<?php
 		require_once("../../lib/head_utils_no_flickity.php");
 		?>
-		<script src="javascript/is.js" type="text/javascript" rel="script"></script>
-		<script src="javascript/eclipsing.js" type="text/javascript" rel="script"></script>
+
 		<link href="css/stylesheet.css" type="text/css" rel="stylesheet"/>
 	</head>
 	<body>
 		<?php require_once("../../lib/navbar.php"); ?>
 
-		<div id="renderPanel">
-
-			<input type='button' id='submitButton' value='Enter Values'/>
-			<input type='button' id='clearButton' value='Clear Graph'/>
-			<input type='button' id='pauseButton' value='Pause'/>
-
-			<input type = 'button' id = 'aDownButton' value = '<--'/>
-			<input type = 'range' id = 'angleSlider' min='0' max = '90'/>
-			<input type = 'button' id = 'aUpButton' value = '-->'/>
-
-			<input type = 'button' id = 'sDownButton' value = '<--'/>
-			<input type = 'range' id = 'separationSlider' min='2' max = '25'/>
-			<input type = 'button' id = 'sUpButton' value = '-->'/>
-
-			<select id='pulldown1'>
-				<option value = "B">B</option>
-				<option value = "A">A</option>
-				<option value = "F">F</option>
-				<option value = "G">G</option>
-				<option value = "K">K</option>
-				<option value = "M">M</option>
-			</select>
-
-			<select id='pulldown2'>
-				<option value = "B">B</option>
-				<option value = "A">A</option>
-				<option value = "F">F</option>
-				<option value = "G">G</option>
-				<option value = "K">K</option>
-				<option value = "M">M</option>
-			</select>
-
-			<canvas id="myCanvas"></canvas>
-		</div>
-
+		<iframe src="eclipsing.html"></iframe>
 	</body>
 </html>
