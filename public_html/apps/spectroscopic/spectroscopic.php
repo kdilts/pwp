ae61964a2eda75f6cfa2791044a95ee022726ffc
Kevin Dilts <kdilts13@gmail.com>
2016-11-27 17:35:52 -0700

    changed spectroscopic to use iframe

--- public_html/apps/spectroscopic/spectroscopic.php
+++ public_html/apps/spectroscopic/spectroscopic.php
@@ -11,14 +11,13 @@
 		<?php
 		require_once("../../lib/head_utils_no_flickity.php");
 		?>
-		<script type="text/javascript" src="javascript/tinycolor.js" rel="script"></script>
-		<script type="text/javascript" src="javascript/is.js" rel="script"></script>
-		<script type="text/javascript" src="javascript/spectroscopic.js" rel="script"></script>
+
 		<link type="text/css" href="css/stylesheet.css" rel="stylesheet"/>
 	</head>
 	<body>
 		<?php
 		require_once("../../lib/navbar.php");
 		?>
+		<iframe src="spectroscopic.html"></iframe>
 	</body>
 </html>
